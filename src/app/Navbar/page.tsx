a7c4c00cfde734c67b215da921b3e23a8b17356a
Zeek <chigereisaac31@gmail.com>
2025-04-22 21:51:34 +0100

    update

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const navbar = () => {
   return (
-    <div>
+    
+    <div className='bg-white text-black p-4 flex flex-row justify-between items-center rounded-b-2xl shadow-md fixed top-0 left-0 w-full z-50'>
       <div>
-        <ul>
-            <Link href={'/'}><li>Home</li></Link>
-        </ul>
+       <Link href='/'><Image src={'/logo1.svg'} alt='logo' width={100} height={100} /></Link> 
+      </div>
+      <div>
+      <ul className='flex justify-between items-center gap-4'>
+        <Link href={'/'}><li>Home</li></Link>
+        <Link href={'/About'}><li>About</li></Link>
+        <Link href={'/Contact'}><li>Contact</li></Link>
+        <Link href={'/Blog'}><li>Blog</li></Link>
+      </ul>
       </div>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import Navbar from './Navbar/page'
+
 
 const page = () => {
   return (
-    <div>
-        <Navbar />
+    <div className='text-white'>
+        <div className='text-center h-screen flex flex-col justify-center items-center'>
+            <h1 className='text-4xl font-bold'>Welcome to my website</h1>
+            <p className='text-lg'>This is a simple website built with Next.js and Tailwind CSS.</p>
+            <p className='text-lg'>You can use this template to create your own website.</p>
+        </div>
+
+
+        
     </div>
   )
 }
